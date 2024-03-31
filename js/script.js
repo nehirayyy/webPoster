36cc3e5bc997be7a609192ea469382bb02cd4443
nehirayyy <nehiray@ocadu.ca>
2024-03-31 17:10:44 -0400

    spotlight emerging
    - going to try to add shadow interaction to the main title as a visual cue for the rest of the site

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
+$(document).ready(function() {
+
+    $(document).on('mousemove', function(e) {
+ 
+        let mouseX= e.pageX
+        let mouseY=e.pageY
+ 
+        $(".spotlight").css({
+            "left": mouseX + "px",
+            "top": mouseY + "px"
+        })
+ 
+    });
+  
+  });
+
+
 console.log("lets see if this remains in your code")
