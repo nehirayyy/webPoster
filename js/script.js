cecb794abd1839b274765965662e4fffebd6b3fa
nehirayyy <nehiray@ocadu.ca>
2024-04-02 13:52:07 -0400

    halfway Didactics
    put most of the interactions aside for a second while I figure out the place for the didactics.
    my current task is to organize my html  to better fit relative positioning.

--- js/script.js
+++ js/script.js
@@ -12,7 +12,17 @@ $(document).ready(function() {
  
     });
   
-  });
 
 
+
+    // $('h1').hover(
+    //     function() {
+    //         $('.spotlight').css({ width: '15rem', height: '50px', transform: 'rotate(90deg)', 'transform-origin': '20% 40%'});
+    //     },
+    //     function() {
+    //         $('.spotlight').css({ width: '15rem', height: '10rem' });
+    //     }
+    // );   NVM LET'S DO THIS ONCE I FIGURE OUT THE DIDACTICS
+
+});
 console.log("lets see if this remains in your code")
